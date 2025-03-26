606ddc9c00d91a02b33e6cd7fb48883f34a3f9a8
Gabe Yoga <g.francke.tech@gmail.com>
2025-03-26 15:32:32 +0100

    enhance CLI to support file input and additional bin packing methods

--- src/cli.py
+++ src/cli.py
@@ -1,7 +1,7 @@
 import argparse
-from typing import Dict, List, Tuple
-from bin_packing_solver import solve_bin_packing
-
+from typing import Dict, List
+from utils import test_solver
+import json
 
 def parse_command_line_arguments() -> Dict:
     parser = argparse.ArgumentParser(description="Simple CLI for bin packing problem")
@@ -9,35 +9,61 @@ def parse_command_line_arguments() -> Dict:
     parser.add_argument(
         "method", 
         type=str, 
-        help="The bin packing algorithm to use. Options: 'first_fit', 'best_fit', 'simulated_annealing'."
+        help="The bin packing algorithm to use. Options: 'first_fit', 'first_fit_decreasing', 'full_bin_packing', 'best_fit', 'simulated_annealing'."
         )
     parser.add_argument(
         "bin_capacity", 
         type=int, 
         help="The maximum capacity of each bin (integer)."
         )
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        "--items",
+        type=int,
+        nargs='+',
+        help="List of item sizes (e.g., 2 5 4 7 1 3 8)"
+    )
+    group.add_argument(
+        "--file",
+        type=str,
+        help="Path to JSON file containing item list"
+    )
     parser.add_argument(
-        "item_list", 
-        type=int, 
-        nargs='+', 
-        help="""A space-separated list of item sizes to be packed (e.g., "2 5 4 7 1 3 8")."""
-        )
-
+        "--display", 
+        action="store_true",
+        help="Display bin contents"
+    )
+    parser.add_argument(
+        "--count", 
+        action="store_true",
+        help="Count full vs. not full bins"
+    )
     args = parser.parse_args()
     return vars(args)
 
-def load_data_from_file(filepath) -> Tuple[int, List[int]]:
-    #TODO Implement file loading logic
-    pass
+def load_data_from_file(filepath: str) -> List[int]:
+    with open(filepath, "r") as f:
+        data = json.load(f)
+    return data["items"]
 
 def main():
+
     args = parse_command_line_arguments()
 
-    result = solve_bin_packing(args['method'], args['bin_capacity'], args['item_list'])
+    if args["items"]:
+        item_list = args["items"]
+    elif args["file"]:
+        item_list = load_data_from_file(args["file"])
+    else:
+        raise ValueError("No input data provided.")
 
-    for i, bin in enumerate(result):
-        print(f"Bin {i + 1}: {bin.item_list}, Free Space: {bin.free_bin_capacity}")
-
+    test_solver(
+        method=args["method"],
+        bin_capacity=args["bin_capacity"],
+        item_list=item_list,
+        display_bins_mode=args["display"],
+        count_bins_mode=args["count"]
+    )
 
 if __name__ == "__main__":
     main()
