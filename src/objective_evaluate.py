68e246047df8eb6b63c4ad85c3797e0f473c14fb
Gabe Yoga <g.francke.tech@gmail.com>
2025-04-08 11:38:43 +0200

    refactor: update evaluate_solution function and reorganize utility functions

--- src/objective_evaluate.py
+++ src/objective_evaluate.py
@@ -1,52 +1,11 @@
-from bin_packing_solver import *
 from typing import List, Tuple
-import timer
-from plots import plot_bins
 from bin import Bin
-
-timer = timer.Timer()
 
 def evaluate_solution(bins: List[Bin]) -> float:
     total_bins = len(bins)
     total_free_space = sum(bin.free_bin_capacity for bin in bins)
 
-    penalty_factor = 0.05
+    penalty_factor = 0.1
     score = total_bins + penalty_factor * total_free_space
 
     return score
-
-def count_bins(bins: list[Bin]):
-    full_bins = 0
-    not_full_bins = 0
-
-    for bin in bins:
-        if bin.free_bin_capacity == 0:
-            full_bins += 1
-        else: 
-            not_full_bins += 1
-
-    return full_bins, not_full_bins
-
-def display_bins(bins: list[Bin]):
-    for i, bin in enumerate(bins):
-        print(f"Bin {i + 1}: {bin.item_list}, Free Space: {bin.free_bin_capacity}")
-
-def test_solver(method: str, 
-                bin_capacity: int, 
-                item_list: List,
-                display_bins_mode: bool = False,
-                count_bins_mode: bool = True,
-                visualize: bool = False) -> list[Bin]:
-    timer.start()
-    bins = solve_bin_packing(method, bin_capacity, item_list)
-    if display_bins_mode:
-        display_bins(bins)
-    if count_bins_mode:
-        full_bins, not_full_bins = count_bins(bins)
-        print(f"Full bins: {full_bins}, Not full bins: {not_full_bins}")
-    if visualize:
-        plot_bins(bins, title=f"Method: {method}, Capacity: {bin_capacity}")
-
-    score = evaluate_solution(bins)
-    print(f"Score: {score}")
-    timer.stop()
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,10 @@
 import time
-from bin_packing_solver import *
-from typing import List, Tuple
+from typing import List, Tuple, Dict
+from numpy import array
+import random
+import numpy as np
 import json
+from bin import Bin
 
 def load_data_from_file(filepath: str) -> List[int]:
     with open(filepath, "r") as f:
@@ -11,3 +14,26 @@ def load_data_from_file(filepath: str) -> List[int]:
 def save_results_to_file(filepath, results):
 
     pass
+
+def generate_items(sequence: array = np.arange(1, 10), number_of_items:int = 100):
+    random.seed(42)
+    item_list = random.choices(sequence, k=number_of_items)
+    
+    return item_list
+
+
+def count_bins(bins: list[Bin]):
+    full_bins = 0
+    not_full_bins = 0
+
+    for bin in bins:
+        if bin.free_bin_capacity == 0:
+            full_bins += 1
+        else: 
+            not_full_bins += 1
+
+    return full_bins, not_full_bins
+
+def display_bins(bins: list[Bin]):
+    for i, bin in enumerate(bins):
+        print(f"Bin {i + 1}: {bin.item_list}, Free Space: {bin.free_bin_capacity}")
